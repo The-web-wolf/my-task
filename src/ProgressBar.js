6640b85c61d8a3dbf8be288e0b0d3ddb578c350e
the-web-wolf <abeshiemmanuel37@gmail.com>
2021-12-13 19:51:28 +0200

    made design...

---
+++ src/ProgressBar.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import "progressbar.css";
+
+class ProgressBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            progress : []
+        };
+    }
+
+    updateProgress(progress){
+        // check if step is in progress
+        if (!this.state.progress.includes(this.props.step)) {
+            this.setState({
+                progress: [...this.state.progress, this.props.step]
+            });
+        }
+    }
+    componentDidMount() {
+        this.updateProgress(this.props.step);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.step !== prevProps.step) {
+            this.updateProgress(this.props.step);
+        }
+        
+    }
+
+    render(){
+        const isShipping = this.state.progress.includes("SHIPPING_STEP");
+        const isBilling  = this.state.progress.includes("BILLING_STEP");
+        const isDetails  = this.state.progress.includes("DETAILS_STEP");
+        let billingComplete, shippingComplete, detailsComplete = '';
+        if (isShipping) {
+            shippingComplete = this.state.progress.length > this.state.progress.indexOf("SHIPPING_STEP") + 1
+        }
+        if (isBilling) {
+            billingComplete = this.state.progress.length > this.state.progress.indexOf("BILLING_STEP") + 1
+        }
+        if (isDetails) {
+            detailsComplete = this.state.progress.length > this.state.progress.indexOf("DETAILS_STEP") + 1
+        }
+
+        return(
+            <div className="progressbar-container">   
+                <ul className="progressbar">
+                    <li className={`progressbar-step ${ isShipping && "active" } ${ shippingComplete && "complete" } `} >
+                        <span className="progressbar-step-tag">Shipping</span>
+                    </li>
+                    <li className={`progressbar-step ${ isBilling? "active" : "" } ${ billingComplete && "complete" } `} >
+                        <span className="progressbar-step-tag">Review & Payments</span>
+                    </li>
+                    <li className={`progressbar-step ${ isDetails ? "active" : "" } ${ detailsComplete && "complete" } `} ></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ProgressBar;
---
+++ src/component/Footer/Footer.component.js
@@ -0,0 +1,9 @@
+import {
+    Footer as SourceFooter,
+} from 'SourceComponent/Footer/Footer.component';
+
+export class Footer extends SourceFooter {
+    // TODO implement logic
+};
+
+export default Footer;
